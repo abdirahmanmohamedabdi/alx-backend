c0662b265b285dff562d88645cd403440a2a4caf
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-27 14:44:33 +0300

    new

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+A basic Flask app
+"""
+from flask import Flask
+from flask import render_template
+from flask import Babel
+
+
+class config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# instantiate the Flask app
+app = Flask(__name__)
+app.config.from_object(config)
+
+# wrap the application with the Babel
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Renders a basic html template
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
